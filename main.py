8c5f33c2140fe377249ece5d5c0affb71a1bb06c
triskattie <tristankoetsier01@outlook.com>
2025-09-21 23:52:02 +0200

    tiny change to make it work in a venv

--- main.py
+++ main.py
@@ -18,8 +18,9 @@ intents.messages = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 # Load previous message IDs
-if os.path.exists("cached.json"):
-    with open("cached.json", "r") as f:
+cached_path = os.path.join(os.path.dirname(__file__), os.pardir, "cached.json")
+if os.path.exists(cached_path):
+    with open(cached_path, "r") as f:
         cached = set(json.load(f))
 else:
     cached = set()
